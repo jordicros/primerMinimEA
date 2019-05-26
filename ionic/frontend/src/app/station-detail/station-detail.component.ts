627b5cfbe573e64b3cf7b302c75a94fa500f3bda
jordicros <36449492+jordicros@users.noreply.github.com>
2019-05-27 00:33:09 +0200

    ionic

--- ionic/frontend/src/app/station-detail/station-detail.component.ts
+++ ionic/frontend/src/app/station-detail/station-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import { BikeListComponent } from '../bike-list/bike-list.component';
 
 @Component({
   selector: 'app-station-detail',
@@ -10,37 +11,58 @@ export class StationDetailComponent implements OnInit {
 
   constructor(private data: DataService) { }
   station
+  stations
   bike
   bikes = [];
   reception
   unassignedBikes = []
   trobat: Boolean = false;
   ngOnInit() {
-    this.station = this.data.station;
-    for(var i=0;i<this.station.bikes.length;i++)
+    this.bikes = []
+    this.unassignedBikes = []
+    this.station = this.data.station
+    this.data.getStations().subscribe(data => {
+      this.stations = data
+      this.data.stations = data
+      console.log(this.station, this.stations)
+      for(let i=0;i<this.data.stations.length;i++){
+        if(this.data.stations[i]._id == this.station._id){this.station = this.data.stations[i]}
+      }
+ for(var i=0;i<this.station.bikes.length;i++)
     {
     this.data.getBikeDetail(this.station.name, this.station.bikes[i]).subscribe(data => { 
        this.bike = data;
-       console.log(this.bike.bike);
       this.bikes.push(this.bike.bike);
- 
+      
     });}
     this.data.getBikes().subscribe(data => {
       this.reception = data
-
-      for(var i=0; i<this.reception.length;i++){
+      let trobat = false;
+      console.log(this.reception, this.bikes)
+      
+        this.unassignedBikes = this.reception;
+      
+      
+      /*for(var i=0; i<this.reception.length;i++){
         for(var j=0; j<this.bikes.length;j++){
-          console.log(this.reception[i], this.bikes[j])
-        if(this.reception[i]._id == this.bikes[j]._id )
-          this.trobat=true;
+          console.log(this.reception[i]._id, this.bikes[j]._id)
+        if(JSON.stringify(this.reception[i]._id) == JSON.stringify(this.bikes[j]._id) ){
+          trobat=true;
+          console.log("no sassigna")
         }
-        if(!this.trobat){
+        else
+          {trobat=false
+          console.log("s'assignaria")}}
+        if(!trobat){
+          console.log("sassigna")
           this.unassignedBikes.push(this.reception[i]);
         }
-        this.trobat = false;
-      }
+        trobat = false;
+      }*/
+      
     });
-    console.log(this.bikes)
+    });
+   
   }
   detallsBike(bike: Object){
     this.data.bike = bike;
